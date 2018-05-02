175b5a2195acfe4051cd3837be7f6fa44a6b64dd
Sj <sanjhapps@gmail.com>
2018-05-02 21:47:27 +0530

    Initial Commit

---
+++ app/src/main/java/com/panic1k/quakereport/EarthquackActivity.java
@@ -0,0 +1,13 @@
+package com.panic1k.quakereport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EarthquackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.earthquack_activity);
+    }
+}
